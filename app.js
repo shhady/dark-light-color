22901ddf5f0be64df91ae629f8c6380a92dfcc3a
shhady <shhadyse@gmail.com>
2022-04-10 21:17:57 +0300

    dark-light-color

---
+++ app.js
@@ -0,0 +1,16 @@
+let color = 'black';
+
+switch(color) {
+    case 'yellow':
+    case 'orange':
+    case 'pink':
+        console.log('light color');
+        break;
+        case 'blue':
+        case 'purple':
+        case 'brown':
+            console.log('dark color');
+            break;
+        default : console.log('Unknown color');
+
+}
